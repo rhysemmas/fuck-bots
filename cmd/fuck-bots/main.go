f3d855ab33de63f2666e1d4f003db488dba434ae
rhysemmas <rhys.emmas@ntlworld.com>
2021-07-12 23:12:05 +0100

    Add logger

--- cmd/fuck-bots/main.go
+++ cmd/fuck-bots/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"go.uber.org/zap"
 )
 
 var (
@@ -18,6 +20,7 @@ func main() {
 	// write to work channel if there is work to do
 	// read from work channel, update playlist name if there is work to do
 	flag.StringVar(&key, "key", defaultKey, "Spotify API key, defaults to value of API_KEY env var")
+	flag.IntVar(&rpm, "key", defaultRpm, "Rate of requests to Spotify API per minute, defaults to 60")
 	flag.Parse()
 
 	if err := exec(); err != nil {
@@ -27,6 +30,20 @@ func main() {
 }
 
 func exec() error {
+	logger, err := initialiseLogger()
+	if err != nil {
+		return fmt.Errorf("unable to initialise logger, %w", err)
+	}
+	defer logger.Sync()
 
 	return nil
+}
+
+func initialiseLogger() (*zap.SugaredLogger, error) {
+	l, err := zap.NewDevelopment()
+	if err != nil {
+		return nil, err
+	}
+
+	return l.Sugar(), nil
 }
